8526d88ac4fa433a08e494ce10d4cf8422184b52
Francuz94 <Forward552@gmail.com>
2022-08-10 11:55:39 +0200

    Implementacja Kontrolera

---
+++ WebApi/Controlers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controlers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _postService.GetAllPosts();
+            return Ok(posts);
+        }
+
+    }
+}
